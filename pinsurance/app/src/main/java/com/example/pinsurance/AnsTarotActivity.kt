cea3bc17b250520604c6dcd77b9b67430115cab6
RP-103 <lecourrierderp@gmail.com>
2022-11-09 16:14:24 +0800

    First version

---
+++ pinsurance/app/src/main/java/com/example/pinsurance/AnsTarotActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pinsurance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnsTarotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ans_tarot)
+    }
+}
