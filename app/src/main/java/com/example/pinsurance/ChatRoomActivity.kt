7bb0da59ebe5a2e4bb9eaae8b7a3e1c6f7a8289e
eating <eating901210@gmail.com>
2022-11-22 19:31:15 +0800

    11/22 語音

--- app/src/main/java/com/example/pinsurance/ChatRoomActivity.kt
+++ app/src/main/java/com/example/pinsurance/ChatRoomActivity.kt
@@ -1,18 +1,85 @@
 package com.example.pinsurance
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ChatRoomActivity : AppCompatActivity() {
+             /** setting mic */
+             // on below line we are creating variables
+            // for text view and image view
+            lateinit var outputTV: TextView
+            lateinit var micIV: ImageView
+
+            // on below line we are creating a constant value
+             private val REQUEST_CODE_SPEECH_INPUT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
 
+
         /** adding back button */
         val display = supportActionBar
         display?.setDisplayHomeAsUpEnabled(true)
+
+
+        /** click mic */
+        // initializing variables of list view with their ids.
+        outputTV = findViewById(R.id.inputMessage)
+        micIV = findViewById(R.id.mic)
+
+        // on below line we are adding on click
+        // listener for mic image view.
+        micIV.setOnClickListener {
+            // on below line we are calling speech recognizer intent.
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            // on below line we are passing language model
+            // and model free form in our intent
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+
+            // on below line we are passing our
+            // language as a default language.
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault()
+            )
+
+            // on below line we are specifying a prompt
+            // message as speak to text on below line.
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+
+            // on below line we are specifying a try catch block.
+            // in this block we are calling a start activity
+            // for result method and passing our result code.
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } catch (e: Exception) {
+                // on below line we are displaying error message in toast
+                Toast
+                    .makeText(
+                        this@ChatRoomActivity, " " + e.message,
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+        }
     }
+
 
     /** clicking back button */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -22,6 +89,31 @@ class ChatRoomActivity : AppCompatActivity() {
                 true
             }
             else ->super.onOptionsItemSelected(item)
+        }
+    }
+
+    /** show the words */
+    // on below line we are calling on activity result method.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // in this method we are checking request
+        // code with our result code.
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            // on below line we are checking if result code is ok
+            if (resultCode == RESULT_OK && data != null) {
+
+                // in that case we are extracting the
+                // data from our array list
+                val res: ArrayList<String> =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                // on below line we are setting data
+                // to our output text view.
+                outputTV.setText(
+                    Objects.requireNonNull(res)[0]
+                )
+            }
         }
     }
 }
